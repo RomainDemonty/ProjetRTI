6a054e1b23373ee9c1f42a65d28b30e1f3f569c0
Romain Demonty <romain.demonty@student.hepl.be>
2023-09-21 13:15:55 +0200

    Debut multi thread du cote serveur

--- Linux_C_CPP/ClientQt/Serveur.cpp
+++ Linux_C_CPP/ClientQt/Serveur.cpp
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "SocketLib.h"
 
-#define DECONNECT 0
+void* FctCaddie(void*);
+
+pthread_t threadCaddie[10];
+
+int nbCaddie = 0;
 
  int main() 
  {  
@@ -15,11 +20,36 @@
 
     sServeur = Socket::ServerSocket(1500);
 
+    for(int i  = 0 ; i<10 ; i++)
+    {
+        threadCaddie[i] = 0;
+    }
+
+
     while(1)
     {
-        sService = Socket::Accept(sServeur,NULL);//Faire une boucle avec plusieurs thread pour accepter
+        if(nbCaddie != 10)
+        {
+            printf("Attente d'un nouuveau client\n");
+            sService = Socket::Accept(sServeur,NULL);//Faire une boucle avec plusieurs thread pour accepter
 
-        
+            for(int i  = 0 ; i<10 ; i++)
+            {
+                if(threadCaddie[i] == 0)
+                {
+                    
+                    pthread_create(&threadCaddie[i], NULL , (void*(*)(void*))FctCaddie,NULL);
+                    i = 11;
+                }
+            }
+            nbCaddie++;
+            printf("Nombre de client connecte: %d\n",nbCaddie);
+        }
+
+
+
+
+        /*
         //Test de Receive
         int result;
         char charReceive[15];
@@ -45,6 +75,17 @@
             }
 
         }
+        */
     }
+}
+
+void*FctCaddie(void *)
+{
+    printf("Hello ceci est un caddie");
+
+    pause();
+
+    nbCaddie--;
+    return 0;
 }
 
