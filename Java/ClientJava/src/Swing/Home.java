ebcb36c50fa627745bee14d34e1ee057aa1ec989
CedGav19 <cedric.gavage@student.hepl.be>
2023-10-14 17:04:38 +0200

    debut clientJava

---
+++ Java/ClientJava/src/Swing/Home.java
@@ -0,0 +1,32 @@
+package Swing;
+
+import javax.swing.*;
+
+public class Home {
+    private JPanel mainPanel;
+    private JPanel connectionPanel;
+    private JPanel MarketPannel;
+    private JTextField textField1;
+    private JButton loginButton;
+    private JButton logoutButton;
+    private JRadioButton subscribeRadioButton;
+    private JPanel BagPannel;
+    private JPanel imagePannel;
+    private JTextField articleTextField;
+    private JTextField priceTextField;
+    private JTextField stockTextField;
+    private JPasswordField passwordField1;
+    private JSpinner quantitySpinner;
+    private JButton leftButton;
+    private JButton rightButton;
+    private JButton chooseButton;
+    private JScrollPane bagScrollPane;
+    private JButton deleteArticleButton;
+    private JButton buyButton;
+    private JButton deleteBagButton;
+    private JList list1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
